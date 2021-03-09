863e88c86e2f40094b65ca979f3ba7e6e0079e18
Arun Sharma <arun@sharma-home.net>
2021-03-08 19:56:24 -0800

    Hookup wal replication callbacks

--- README.md
+++ README.md
@@ -21,10 +21,10 @@ Next:
 
 * Register rust functions to receive WAL
 * This would involve adding some new commands to shell.c
-  * ```sqlite> register wal```
-  * ```sqlite> pragma wal_mode=leader```
-  * ```sqlite> pragma wal_mode=follower```
+  * ```sqlite>.wal_leader```
+  * ```sqlite>.wal_follower```
 * Implement rust code for various callbacks from C
+* Plugin one of the Raft implementation where LogEntry is a wal frame
 * Run three copies of rust_wal one as leader, two as follower
 * Verify correctness when leader is killed and followers take over
 
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,58 @@
-use rust_wal::shell_main;
-
 use std::env;
-use std::os::raw::c_int;
 use std::os::raw::c_char;
+use std::os::raw::c_int;
+use std::os::raw::c_void;
+
+use rust_wal;
+
+extern "C" fn begin_tx(_wal: *mut rust_wal::sqlite3_wal_replication, _arg: *mut c_void) -> i32 {
+    println!("Got tx begin");
+    return 0;
+}
+
+extern "C" fn abort_tx(_wal: *mut rust_wal::sqlite3_wal_replication, _arg: *mut c_void) -> i32 {
+    println!("Got tx abort");
+    return 0;
+}
+
+// int (*xFrames)(sqlite3_wal_replication*, void *pArg, int szPage, int nFrame,
+//                sqlite3_wal_replication_frame *aFrame, unsigned nTruncate, int isCommit);
+extern "C" fn frames(
+    _wal: *mut rust_wal::sqlite3_wal_replication,
+    _arg: *mut c_void,
+    _sz_page: i32,
+    _num_frames: i32,
+    _frame_array: *mut rust_wal::sqlite3_wal_replication_frame,
+    _ntrunc: u32,
+    _is_commit: i32,
+) -> i32 {
+    println!("Got frames");
+    return 0;
+}
+
+extern "C" fn undo_tx(_wal: *mut rust_wal::sqlite3_wal_replication, _arg: *mut c_void) -> i32 {
+    println!("Got undo tx");
+    return 0;
+}
+
+extern "C" fn end_tx(_wal: *mut rust_wal::sqlite3_wal_replication, _arg: *mut c_void) -> i32 {
+    println!("Got end tx");
+    return 0;
+}
 
 fn main() {
     let argv: Vec<_> = env::args().collect();
-    let args:Vec<_> = argv.into_iter().map(|arg| { arg.as_ptr() } ).collect();
+    let args: Vec<_> = argv.into_iter().map(|arg| arg.as_ptr()).collect();
+    let mut wal_replication: *mut rust_wal::sqlite3_wal_replication;
+
     unsafe {
-        shell_main(args.len() as c_int, args.as_ptr() as *mut *mut c_char);
+        wal_replication = rust_wal::get_wal_replication();
+        (*wal_replication).xBegin = Some(begin_tx);
+        (*wal_replication).xAbort = Some(abort_tx);
+        (*wal_replication).xFrames = Some(frames);
+        (*wal_replication).xUndo = Some(undo_tx);
+        (*wal_replication).xEnd = Some(end_tx);
+        rust_wal::sqlite3_wal_replication_register(wal_replication, 1);
+        rust_wal::shell_main(args.len() as c_int, args.as_ptr() as *mut *mut c_char);
     }
 }
